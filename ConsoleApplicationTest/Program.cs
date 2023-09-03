60676dbfe38ed5bed8256a2e63b1058ff9e56074
matti <mattikjellstadli@icloud.com>
2023-09-03 21:07:16 +0200

    User class added

--- ConsoleApplicationTest/Program.cs
+++ ConsoleApplicationTest/Program.cs
@@ -4,8 +4,33 @@ namespace ConsoleApplicationTest
 {
     internal class Program
     {
-        public static void Main(string[] args) {
-            Console.WriteLine("Hello You fool");
+        public static void Main(string[] args)
+        {
+            User user = new User("Mike", 34);
+            Console.WriteLine(user.GetName() + user.GetAge());
+        }
+    }
+
+    public class User
+    {
+        private string _name;
+
+        private int _age;
+
+        public User(string name, int age)
+        {
+            this._name = name;
+            this._age = age;
+        }
+        
+        public string GetName()
+        {
+            return this._name;
+        }
+        
+        public int GetAge()
+        {
+            return this._age;
         }
     }
 }
